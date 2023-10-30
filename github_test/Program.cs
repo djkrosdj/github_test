7e3e8301fe61755c62303b57876239d3f82132bf
Kros <djkros@inbox.ru>
2023-10-30 08:13:01 +0300

    feat: add quotient of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -14,7 +14,8 @@ namespace GitTest
                 Console.WriteLine($"Выберите команду: \n" +
                                   $"1. Сложить числа \n" +
                                   $"2. Вычесть числа \n" +
-                                  $"3. Умножить числа \n");
+                                  $"3. Умножить числа \n" +
+                                  $"4. Поделить числа \n");
 
                 var choice = Convert.ToInt32(Console.ReadLine());
 
@@ -39,6 +40,12 @@ namespace GitTest
                     {
                         var product = firstNumber * secondNumber;
                         Console.WriteLine("Результат умножения " + product);
+                        break;
+                    }
+                    case 4:
+                    {
+                        var quotient = firstNumber - secondNumber;
+                        Console.WriteLine("Результат деления " + quotient);
                         break;
                     }
                 }
